09ddf057924d02692db31249108e992a1601cc6b
aboumata <boumatabibon@gmail.com>
2025-04-21 19:25:38 +0100

    so_long

---
+++ libft/ft_putnbr_fd.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboumata <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 18:09:51 by aboumata          #+#    #+#             */
+/*   Updated: 2025/04/21 18:09:52 by aboumata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+    if (fd < 0)
+        return ;
+    write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+    if (fd < 0)
+        return ;
+    if (s == NULL)
+        return ;
+    write(fd, s, ft_strlen(s));
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+    if (fd < 0)
+        return ;
+    if (n == -2147483648)
+    {
+        ft_putstr_fd("-2147483648", fd);
+        return ;
+    }
+    if (n < 0)
+    {
+        ft_putchar_fd('-', fd);
+        n = -n;
+    }
+    if (n >= 10)
+        ft_putnbr_fd(n / 10, fd);
+    ft_putchar_fd(n % 10 + '0', fd);
+}
